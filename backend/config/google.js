87e5d9d266933f836ba94ba3c95d6b44ce192a1a
Game-Page <www.mohamedasvath@gmail.com>
2025-12-20 15:59:48 +0530

    Fix Google OAuth callback config

--- backend/config/google.js
+++ backend/config/google.js
@@ -11,7 +11,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+
+      // 🔥 IMPORTANT CHANGES
+      callbackURL: "/api/auth/google/callback",
+      proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       return done(null, {
